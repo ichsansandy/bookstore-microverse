ba686b521972980f3aa314867b6f4768e747b2ec
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-07-12 19:51:01 +0700

    Implement redux

--- src/components/BookListContainer.js
+++ src/components/BookListContainer.js
@@ -1,66 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import BookCard from './BookCard';
 
 function BookListContainer() {
-  const [bookList, setBookList] = useState({
-    item1: [
-      {
-        title: 'The Lord of the Rings',
-        author: 'J.R.R. Tolkien',
-        category: 'Fiction',
-      },
-    ],
-    item2: [
-      {
-        title: 'Fahrenheit 451',
-        author: 'Ray Bradbury',
-        category: 'Fiction',
-      },
-    ],
-    item3: [
-      {
-        title: 'Frankenstein ',
-        author: 'Mary Shelley',
-        category: 'Fiction',
-      },
-    ],
-    item4: [
-      {
-        title: 'The Great Gatsby ',
-        author: 'F. Scott Fitzgerald',
-        category: 'Fiction',
-      },
-    ],
-  });
-
-  const deleteBookList = (book) => {
-    const newItems = { ...bookList };
-    delete newItems[book]; // or whichever key you want
-    setBookList(newItems);
-  };
+  const bookList = useSelector((state) => state.books.value);
 
   return (
-    <div className="book-container">
-      {Object.keys(bookList).map((book) => (
-        <div className="book-card" key={book}>
-          <div className="book-category">
-            {bookList[book][0].category}
-          </div>
-          <div className="book-title">
-            {bookList[book][0].title}
-          </div>
-          <div className="book-author">
-            {bookList[book][0].author}
-          </div>
-          <button
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              deleteBookList(book);
-            }}
-          >
-            Delete
-          </button>
-        </div>
+    <div className="book-container" key="book-container">
+      {bookList.map((book) => (
+        <BookCard book={book} key={book.item_id} />
       ))}
       <hr />
     </div>
